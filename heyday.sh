49c6c7a5d7588362e8b2e1a26c120f56503b3ab8
Agl0032 <114427956+Agl0032@users.noreply.github.com>
2022-10-12 22:30:26 -0500

    Create heyday.sh

---
+++ heyday.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#pronmpt to enter name
+echo "Hi! what's your name?"
+if [ $# -eq 1 ]; then
+echo "..."
+echo "Hey $1 :)"
+else
+echo "..."
+echo "Could you type that after the .sh? I couldn't hear you"
+fi
